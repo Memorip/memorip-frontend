a4b62899ca540591e2179c304a583ff8bd87f808
HyungWookChoi <qpflapffhs76@gmail.com>
2023-06-17 23:24:14 +0900

    :art: 컴포넌트 분리

---
+++ src/app/plan/[slug]/components/TimeLine.tsx
@@ -0,0 +1,13 @@
+const TimeLine = () => {
+  return (
+    <div className='flex items-center justify-between rounded-lg bg-zinc-100 p-4'>
+      <div className='flex flex-col justify-between gap-4'>
+        <span className='text-base font-bold'>제주 OO 호텔</span>
+        <span className='text-xs font-semibold'>휴식</span>
+      </div>
+      <span className='text-xs font-semibold text-stone-400'>10:00 AM</span>
+    </div>
+  )
+}
+
+export default TimeLine
