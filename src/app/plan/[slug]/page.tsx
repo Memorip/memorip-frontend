ba09b5284f7ae2673c58e34413230dc0991aa54e
HyungWookChoi <qpflapffhs76@gmail.com>
2023-06-21 21:47:18 +0900

    :lipstick: (frontend): 검색 페이지 마크업

--- src/app/plan/[slug]/page.tsx
+++ src/app/plan/[slug]/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 import DateButton from '@/app/plan/[slug]/components/DateButton'
 import ProgressDot from '@/app/plan/[slug]/components/ProgressDot'
 import TimeLine from '@/app/plan/[slug]/components/TimeLine'
 import TripInfoCard from '@/app/plan/[slug]/components/TripInfoCard'
+
+import ROUTE from '@/constants/route'
 
 const TIMELINE_LENGTH = 4
 
@@ -26,12 +30,13 @@ export default function PlanDetail() {
           {new Array(TIMELINE_LENGTH).fill(0).map((_, index) => (
             <TimeLine key={index} />
           ))}
-          <div className='flex h-[88px] w-full items-center justify-center rounded-lg bg-zinc-100 p-4'>
-            <div className='flex items-center gap-1'>
-              <i className='ri-add-line text-lg font-bold text-emerald-500' />
-              <span className='text-sm font-bold text-emerald-500'>일정 추가하기</span>
-            </div>
-          </div>
+          <Link
+            className='flex h-[88px] w-full items-center justify-center rounded-lg bg-zinc-100 p-4'
+            href={ROUTE.SEARCH}
+          >
+            <i className='ri-add-line text-lg font-bold text-emerald-500' />
+            <span className='text-sm font-bold text-emerald-500'>일정 추가하기</span>
+          </Link>
         </div>
       </div>
     </div>
