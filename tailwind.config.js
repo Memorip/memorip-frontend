ab8dd0c0808398bd733a0668f923cd84a27925c4
HyungWookChoi <qpflapffhs76@gmail.com>
2023-06-21 21:07:09 +0900

    :lipstick: (frontend): slide animation 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,16 @@ module.exports = {
     extend: {
       animation: {
         'slide-up': 'slideUp 0.5s ease-in-out',
+        'slide-left': 'slideLeft 0.5s ease-in-out',
       },
       keyframes: {
         slideUp: {
           '0%': { transform: 'translateY(100%)' },
           '100%': { transform: 'translateY(0)' },
+        },
+        slideLeft: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
         },
       },
       fontFamily: {
