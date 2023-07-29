bd2899909896b0d28a14d5674936ca1bb3720ca6
HyungWookChoi <qpflapffhs76@gmail.com>
2023-07-29 19:07:58 +0900

    :sparkles: 다가오는 여행, 내 여행 기능 추가

--- src/components/views/me/components/Travel.tsx
+++ src/components/views/me/components/Travel.tsx
@@ -18,6 +18,9 @@ const Travel = () => {
 
   if (!myPlanQuery.isSuccess) return null
 
+  const upComingPlans = myPlanQuery.data.filter(({ startDate }) => dayjs(startDate).isAfter(dayjs()))
+  const pastPlans = myPlanQuery.data.filter(({ startDate }) => dayjs(startDate).isBefore(dayjs()))
+
   return (
     <Tab.Panel className={clsx('flex h-full flex-col items-center justify-center')}>
       {myPlanQuery.data.length > 0 ? (
@@ -31,30 +34,58 @@ const Travel = () => {
               </Link>
             </div>
           </div>
-          <div className='p-4'>
-            <span className='mb-4 inline-block text-base font-bold'>다가오는 여행</span>
-            <div className='flex flex-col gap-5'>
-              {myPlanQuery.data.map(({ id, city, startDate, endDate }) => (
-                <Link href={ROUTE.PLAN(id)} className='flex items-center gap-4' key={id}>
-                  <Image
-                    className='rounded-full border border-gray-100 drop-shadow-sm'
-                    src={'/images/bear.jpeg'}
-                    width={60}
-                    height={60}
-                    alt='profile'
-                  />
-                  <div className='flex flex-col gap-1'>
-                    <span className='text-sm font-bold'>{city.join(',')}</span>
-                    <span className='text-xs font-semibold text-gray-500'>
-                      {dayjs(startDate).format('YYYY.MM.DD')} ~ {dayjs(endDate).format('YYYY.MM.DD')}
-                    </span>
-                    <span className='text-xs font-semibold text-gray-400'>{city.length}개 도시</span>
-                  </div>
-                  <i className='ri-more-fill ml-auto text-xl' />
-                </Link>
-              ))}
+          {upComingPlans.length > 0 && (
+            <div className='p-4'>
+              <span className='mb-4 inline-block text-base font-bold'>다가오는 여행</span>
+              <div className='flex flex-col gap-5'>
+                {upComingPlans.map(({ id, city, startDate, endDate }) => (
+                  <Link href={ROUTE.PLAN(id)} className='flex items-center gap-4' key={id}>
+                    <Image
+                      className='rounded-full border border-gray-100 drop-shadow-sm'
+                      src={'/images/bear.jpeg'}
+                      width={60}
+                      height={60}
+                      alt='profile'
+                    />
+                    <div className='flex flex-col gap-1'>
+                      <span className='text-sm font-bold'>{city.join(',')}</span>
+                      <span className='text-xs font-semibold text-gray-500'>
+                        {dayjs(startDate).format('YYYY.MM.DD')} ~ {dayjs(endDate).format('YYYY.MM.DD')}
+                      </span>
+                      <span className='text-xs font-semibold text-gray-400'>{city.length}개 도시</span>
+                    </div>
+                    <i className='ri-more-fill ml-auto text-xl' />
+                  </Link>
+                ))}
+              </div>
             </div>
-          </div>
+          )}
+          {pastPlans.length > 0 && (
+            <div className='p-4'>
+              <span className='mb-4 inline-block text-base font-bold'>지난 여행</span>
+              <div className='flex flex-col gap-5'>
+                {pastPlans.map(({ id, city, startDate, endDate }) => (
+                  <Link href={ROUTE.PLAN(id)} className='flex items-center gap-4' key={id}>
+                    <Image
+                      className='rounded-full border border-gray-100 drop-shadow-sm'
+                      src={'/images/bear.jpeg'}
+                      width={60}
+                      height={60}
+                      alt='profile'
+                    />
+                    <div className='flex flex-col gap-1'>
+                      <span className='text-sm font-bold'>{city.join(',')}</span>
+                      <span className='text-xs font-semibold text-gray-500'>
+                        {dayjs(startDate).format('YYYY.MM.DD')} ~ {dayjs(endDate).format('YYYY.MM.DD')}
+                      </span>
+                      <span className='text-xs font-semibold text-gray-400'>{city.length}개 도시</span>
+                    </div>
+                    <i className='ri-more-fill ml-auto text-xl' />
+                  </Link>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       ) : (
         <div className='flex flex-col items-center justify-center'>
