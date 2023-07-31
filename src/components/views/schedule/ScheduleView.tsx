b2e3a773096b0b7430539d412fe1aa8d67c7ca45
HyungWookChoi <qpflapffhs76@gmail.com>
2023-07-31 23:46:54 +0900

    :lipstick: 도시 선택 목 데이터 국내 도시로 수정

--- src/components/views/schedule/ScheduleView.tsx
+++ src/components/views/schedule/ScheduleView.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import React from 'react'
 
+import clsx from 'clsx'
+
 import { Input } from '@/components/forms'
-import Avatar from '@/components/shared/Avatar'
 
 import ROUTE from '@/constants/route'
 import { usePlan } from '@/hooks/usePlan'
@@ -11,17 +13,26 @@ import { usePlan } from '@/hooks/usePlan'
 interface City {
   id: number
   name: string
+  description: string
+  image: string
   selected: boolean
 }
 
 const ScheduleView = () => {
   const MOCK = [
-    { id: 1, name: '도쿄', selected: false },
-    { id: 2, name: '하코네', selected: false },
-    { id: 3, name: '요코하마', selected: false },
-    { id: 4, name: '가마쿠라', selected: false },
-    { id: 5, name: '오사카', selected: false },
-    { id: 6, name: '나고야', selected: false },
+    { id: 1, name: '국내', description: '어디론가 떠나고 싶을 때', image: '/images/국내.jpg', selected: false },
+    { id: 2, name: '가평·양평', description: '가평,양평', image: '/images/가평.jpg', selected: false },
+    { id: 3, name: '강릉·속초', description: '강릉,속초,양양', image: '/images/강릉.jpg', selected: false },
+    { id: 4, name: '경주', description: '경주', image: '/images/경주.jpg', selected: false },
+    { id: 5, name: '부산', description: '부산', image: '/images/부산.jpg', selected: false },
+    { id: 6, name: '여수', description: '여수,순천', image: '/images/여수.jpg', selected: false },
+    { id: 7, name: '인천', description: '인천,강화도', image: '/images/인천.jpg', selected: false },
+    { id: 8, name: '전주', description: '전주,군산', image: '/images/전주.jpg', selected: false },
+    { id: 9, name: '제주', description: '제주', image: '/images/제주.jpg', selected: false },
+    { id: 10, name: '춘천·홍천', description: '춘천,홍천', image: '/images/춘천.jpg', selected: false },
+    { id: 11, name: '태안', description: '태안,당진,서산', image: '/images/태안.jpg', selected: false },
+    { id: 12, name: '통영·거제·남해', description: '통영,거제,남해', image: '/images/통영.jpg', selected: false },
+    { id: 13, name: '포항·안동', description: '포항,안동', image: '/images/포항.jpg', selected: false },
   ]
 
   const [selectedCity, setSelectedCity] = React.useState<City[]>([])
@@ -48,7 +59,7 @@ const ScheduleView = () => {
   }
 
   return (
-    <div className='relative h-full w-full p-4 '>
+    <div className={clsx('relative h-full overflow-y-auto p-4', selectedCity.length > 0 && 'h-[calc(100vh-154px)]')}>
       <div className='relative w-full'>
         <div className='absolute inset-y-0 right-5 flex items-center'>
           <i className='ri-search-line' />
@@ -59,10 +70,12 @@ const ScheduleView = () => {
         {MOCK.map((city) => (
           <div className='flex items-center space-x-4' key={city.id}>
             <div className='flex'>
-              <Avatar size={40} />
+              <div className='relative h-10 w-10'>
+                <Image className='rounded-full' src={city.image} fill alt={city.name} sizes='100vw' priority />
+              </div>
               <div className='ml-2 flex flex-col'>
                 <span className='font-semibold'>{city.name}</span>
-                <p className='text-sm text-gray-500'>도쿄, 하코네,요코하마,가마쿠라</p>
+                <p className='text-sm text-gray-500'>{city.description}</p>
               </div>
             </div>
 
@@ -86,21 +99,22 @@ const ScheduleView = () => {
           </div>
         ))}
       </div>
-
       {selectedCity.length > 0 && (
-        <div className='absolute h-[30vh] w-full border-t border-slate-100 p-5'>
-          <div className='flex w-fit items-center space-x-3'>
+        <div className='fixed bottom-0 left-0 w-full border-t border-slate-100 bg-white'>
+          <div className='flex w-full items-center space-x-3 overflow-x-auto px-4 py-3'>
             {selectedCity.map((city, idx) => (
               <>
-                <div key={city.id} className='flex flex-col items-center'>
-                  <span
-                    className='relative left-4 top-3 flex h-[20px] w-[20px] items-center justify-center rounded-full border bg-white'
-                    onClick={() => handleSelect(city.id)}
-                  >
-                    <i className='ri-close-line text-xs text-gray-500' />
-                  </span>
-                  <Avatar size={40} />
-                  <span className='flex-none text-xs'>{city.name}</span>
+                <div key={city.id} className='flex w-12 flex-col items-center bg-white'>
+                  <div className='relative mb-1 h-10 w-10'>
+                    <button
+                      className='absolute right-[-4px] top-[-4px] z-10 flex h-[20px] w-[20px] items-center justify-center rounded-full border bg-white'
+                      onClick={() => handleSelect(city.id)}
+                    >
+                      <i className='ri-close-line text-xs text-gray-500' />
+                    </button>
+                    <Image className='rounded-full' src={city.image} fill alt={city.name} sizes='100vw' priority />
+                  </div>
+                  <span className='flex-none truncate text-xs'>{city.name}</span>
                 </div>
                 {idx !== selectedCity.length - 1 && <i className='ri-arrow-right-line' />}
               </>
@@ -109,7 +123,7 @@ const ScheduleView = () => {
 
           <Link href={ROUTE.SCHEDULE_PLAN}>
             <button
-              className='mt-5 flex w-full flex-none items-center justify-center rounded-md bg-blue-500 py-4 text-sm font-medium text-white'
+              className='flex w-full flex-none items-center justify-center bg-blue-500 py-4 text-sm font-medium text-white'
               onClick={handleSetPlan}
             >
               {`${
